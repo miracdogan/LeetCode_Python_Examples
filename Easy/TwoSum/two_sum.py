1b262fdad8f6e8106a3808e1267b30f3ff9a6bd7
Mirac Dogan <miracdgn@outlook.com>
2020-05-25 14:38:02 +0300

    Easy
    Answer

---
+++ Easy/TwoSum/two_sum.py
@@ -0,0 +1,12 @@
+nums = [2, 7, 11, 15]
+target = 9
+
+class Solution:
+    def twoSum(self, nums, target):
+        
+        
+        for i in range (len(nums)):
+            for j in range (i+1, len(nums)):
+                sum = nums[j] + nums[i]
+                if sum == target:
+                    return [i, j]
